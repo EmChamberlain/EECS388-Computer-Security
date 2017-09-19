c3be4293b83c9539b1c7219e992ccc5a56e656dd
MattChamberlain <Mattchamberlain2@gmail.com>
2017-09-19 18:53:20 -0400

    Test for part 1

---
+++ Project_1/Part_1/len_ext_attack.py
@@ -0,0 +1,16 @@
+from pymd5 import md5
+from pymd5 import padding
+
+m = "Use HMAC, not hashes"
+h = md5()
+h.update(m)
+print h.hexdigest()
+
+h = md5(state="3ecc68efa1871751ea9b0b1a5b25004d".decode("hex"), count=512)
+x = "Good advice"
+h.update(x)
+print h.hexdigest()
+
+h = md5()
+h.update(m + padding(len(m)*8) + x)
+print h.hexdigest()
