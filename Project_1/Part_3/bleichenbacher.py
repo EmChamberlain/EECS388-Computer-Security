f289a33d2f2c42b32d6a56c69422bd0d2206a857
MattSurface <Mattchamberlain2@gmail.com>
2017-09-27 16:24:35 -0400

    Finished writup and part 3

--- Project_1/Part_3/bleichenbacher.py
+++ Project_1/Part_3/bleichenbacher.py
@@ -4,30 +4,14 @@ import sys
 import math
 from bisect import bisect
 
-def trailing(n):
-    """Count the number of trailing zero bits in abs(n)."""
-    if not n:
-        return 0
-    t = 0
-    while not n & 1:
-        n >>= 1
-        t += 1
-    return t
-
-def bitcount(n):
-    """Calculate bit size of the nonnegative integer n."""
-    bc = bisect(powers, n)
-    if bc != 300:
-        return bc
-    bc = int(math.log(n, 2)) - 4
-    return bc + bctable[n>>bc]
 
 
 
 
-# message = sys.argv[1]
-message = "eecs388+jdoe+1.23"
-
+message = sys.argv[1]
+# message = "eecs388+jdoe+1.23"
+if message[0] == '\"':
+    message[1:1]
 e = 3
 
 msg_beg = "0001FF003031300d060960864801650304020105000420"
@@ -46,7 +30,7 @@ while (forged_signature**e < int(msg, 16)):
 
 
 hex_str_sig = "%0512x"%(forged_signature**e)
-print msg[:110]
-print hex_str_sig[:110]
+# print msg[:110]
+# print hex_str_sig[:110]
 
 print integer_to_base64(forged_signature)
