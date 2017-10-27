e7345504ec9471002de4a8c99b189940dd8781ea
MattChamberlain <Mattchamberlain2@gmail.com>
2017-10-27 03:10:30 -0400

    Debugged and tested detector.py ready to be turned in

--- Project_3/detector.py
+++ Project_3/detector.py
@@ -1,5 +1,6 @@
 import dpkt
 import sys
+import socket
 
 
 
@@ -10,27 +11,46 @@ def exists(ips, ip):
     return False
 
 def main():
-    f = open(sys.argv[1])
+    f = open(sys.argv[1], "rb")
     pcap = dpkt.pcap.Reader(f)
 
     ips = {}
 
+
     for ts, buf in pcap:
-        eth = dpkt.ethernet.Ethernet(buf)
+        eth = None
+        ip = None
+        tcp = None
+        syn_flag = None
+        ack_flag = None
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+        except:
+            continue
+        if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+            continue
         ip = eth.data
+        if ip.p != dpkt.ip.IP_PROTO_TCP:
+            continue
+        else:
+            pass
         tcp = ip.data
+
+
         syn_flag = (tcp.flags & dpkt.tcp.TH_SYN) != 0
         ack_flag = (tcp.flags & dpkt.tcp.TH_ACK) != 0
+        pass
         if syn_flag and not ack_flag:
             if exists(ips, ip.src):
                 ips[ip.src][0] += 1
             else:
-                ips[ip.src] = (1, 0)
+                ips[ip.src] = [1, 0]
         if syn_flag and ack_flag:
             if exists(ips, ip.dst):
                 ips[ip.dst][1] += 1
             else:
-                ips[ip.dst] = (0, 1)
+                ips[ip.dst] = [0, 1]
+
 
     suspect = []
 
@@ -39,6 +59,10 @@ def main():
         syn_ack_count = ips[ip][1]
         if syn_count >= 3*syn_ack_count:
             suspect.append(ip)
+
+    for ip in suspect:
+        print socket.inet_ntoa(ip)
+
 
 
 
